49cfc0b925d6d8588e585a3b6927c1cd17456b1e
Niharika <niharika.singh6@byjus.com>
2022-07-07 11:25:25 +0530

    feature/get slots for a user

---
+++ api/userSlot.go
@@ -0,0 +1,16 @@
+package api
+
+import (
+	"Slot_booking/start_up"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func UserSlot(ctx *gin.Context) {
+	slots, err := start_up.BookingController.GetUserSlot(ctx)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	} else {
+		ctx.JSON(http.StatusOK, gin.H{"Slots": slots})
+	}
+}
