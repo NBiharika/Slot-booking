46de9ce7cdfbcc6c3554ce91ecdb61fe676274c8
Niharika <niharika.singh6@byjus.com>
2022-07-07 11:10:28 +0530

    feature/Initialization

---
+++ start_up/start_up.go
@@ -0,0 +1,38 @@
+package start_up
+
+import (
+	"Slot_booking/controller"
+	"Slot_booking/manager"
+	"Slot_booking/service"
+)
+
+var (
+	slotRepository manager.SlotRepository
+	slotService    service.SlotService
+	SlotController controller.SlotController
+
+	userRepository manager.UserRepository
+	userService    service.UserService
+	UserController controller.UserController
+
+	bookingRepository manager.BookingRepository
+	bookingService    service.BookingService
+	BookingController controller.BookingController
+)
+
+//values are assigned
+func Initialize() {
+	manager.InitilizeConnection()
+	slotRepository = manager.SlotRepo()
+	slotService = service.NewSlotService(slotRepository)
+	SlotController = controller.NewSlotController(slotService)
+
+	userRepository = manager.UserRepo()
+	userService = service.NewUserService(userRepository)
+	UserController = controller.NewUserController(userService)
+
+	bookingRepository = manager.BookingRepo()
+	bookingService = service.NewService(bookingRepository)
+	BookingController = controller.New(bookingService, slotService, userService)
+
+}
