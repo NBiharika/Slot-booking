49fc6ef1109e9b90fb2cae667f566610e71ee145
Niharika <niharika.singh6@byjus.com>
2022-07-07 10:26:47 +0530

    feature/get the slot

---
+++ api/addSlot.go
@@ -0,0 +1,17 @@
+package api
+
+import (
+	"Slot_booking/start_up"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func AddSlot(ctx *gin.Context) {
+	err := start_up.SlotController.Save(ctx)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	} else {
+		ctx.JSON(http.StatusOK, gin.H{"message": "Slot input is valid"})
+	}
+
+}
