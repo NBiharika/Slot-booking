0e2d01439db4637c092614bced5a62cbd8db7d6a
Niharika <niharika.singh6@byjus.com>
2022-07-07 11:00:26 +0530

    feature/bookingService

---
+++ service/bookingService.go
@@ -0,0 +1,40 @@
+package service
+
+import (
+	"Slot_booking/entity"
+	"Slot_booking/manager"
+)
+
+type BookingService interface {
+	Save(booking entity.Booking) (entity.Booking, error)
+	FindAll() []entity.Booking
+	Cancel(booking entity.Booking) error
+	GetUserSlot(userID uint64) ([]entity.Booking, error)
+}
+
+type bookingservice struct {
+	bookings manager.BookingRepository
+}
+
+func NewService(repo manager.BookingRepository) BookingService {
+	return &bookingservice{
+		bookings: repo,
+	}
+}
+
+func (service *bookingservice) Save(booking entity.Booking) (entity.Booking, error) {
+	err := service.bookings.Save(booking)
+	return booking, err
+}
+
+func (service *bookingservice) Cancel(booking entity.Booking) error {
+	err := service.bookings.Cancel(booking)
+	return err
+}
+
+func (service *bookingservice) FindAll() []entity.Booking {
+	return service.bookings.FindAll()
+}
+func (service *bookingservice) GetUserSlot(userID uint64) ([]entity.Booking, error) {
+	return service.bookings.GetUserSlot(userID)
+}
