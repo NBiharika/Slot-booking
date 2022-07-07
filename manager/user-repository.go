7b882b2f3ee8a563c933527fab6f15e357e72a59
Niharika <niharika.singh6@byjus.com>
2022-07-07 10:52:21 +0530

    feature/User Repository

---
+++ manager/user-repository.go
@@ -0,0 +1,34 @@
+package manager
+
+import (
+	"Slot_booking/entity"
+	"gorm.io/gorm"
+)
+
+type UserRepository interface {
+	Save(user entity.User) error
+	Find(userID uint64) (entity.User, error)
+}
+
+type UserDB struct {
+	connection *gorm.DB
+}
+
+func UserRepo() UserRepository {
+	return &UserDB{
+		connection: dbClient,
+	}
+}
+
+func (db *UserDB) Save(user entity.User) error {
+	//db.connection.AutoMigrate(&entity.User{})
+	err := db.connection.Create(&user).Error
+	return err
+}
+
+func (db *UserDB) Find(userID uint64) (entity.User, error) {
+	var user entity.User
+	user.ID = userID
+	err := db.connection.First(&user).Error
+	return user, err
+}
