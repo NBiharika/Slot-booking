c3266fa7b455bd309108937fba45a8d58befcd20
Niharika <niharika.singh6@byjus.com>
2022-07-07 10:51:41 +0530

    feature/Slot Repository

---
+++ manager/slot-repository.go
@@ -0,0 +1,56 @@
+package manager
+
+import (
+	"Slot_booking/entity"
+	"gorm.io/gorm"
+)
+
+type SlotRepository interface {
+	Save(slot entity.Slot) error
+	CreateSlot(string) error
+	FindAll() []entity.Slot
+	Find(slot entity.Slot) (entity.Slot, error)
+	GetSlots(slot entity.Slot) (entity.Slot, error)
+}
+
+type SlotDB struct {
+	connection *gorm.DB
+}
+
+func SlotRepo() SlotRepository {
+	return &SlotDB{
+		connection: dbClient,
+	}
+}
+
+func (db *SlotDB) CreateSlot(startTime string) error {
+	var slot entity.Slot
+	slot.Date = entity.DateForSlot()
+	slot.StartTime = startTime
+	response := dbClient.Create(&slot)
+	if response.Error != nil {
+		return response.Error
+	}
+	return nil
+}
+
+func (db *SlotDB) Save(slot entity.Slot) error {
+	err := db.connection.Create(&slot).Error
+	//db.connection.AutoMigrate(&entity.Slot{})
+	return err
+}
+
+func (db *SlotDB) FindAll() []entity.Slot {
+	var slot []entity.Slot
+	db.connection.Find(&slot)
+	return slot
+}
+
+func (db *SlotDB) Find(slot entity.Slot) (entity.Slot, error) {
+	err := db.connection.Where(&slot).Find(&slot).Error
+	return slot, err
+}
+func (db *SlotDB) GetSlots(slot entity.Slot) (entity.Slot, error) {
+	err := db.connection.Where(&slot).Find(&slot).Error
+	return slot, err
+}
