bea03964f599cc76f83d3d066acbb099be5a43ce
Niharika <niharika.singh6@byjus.com>
2022-07-07 10:58:56 +0530

    feature/slotService

---
+++ service/slotService.go
@@ -0,0 +1,46 @@
+package service
+
+import (
+	"Slot_booking/entity"
+	"Slot_booking/manager"
+)
+
+type SlotService interface {
+	Save(booking entity.Slot) (entity.Slot, error)
+	FindAll() []entity.Slot
+	Find(startTime, date string) (entity.Slot, error)
+	GetSlots(slotID uint64) (entity.Slot, error)
+	//CloseDB()
+}
+
+type service struct {
+	slots manager.SlotRepository
+}
+
+func NewSlotService(repo manager.SlotRepository) SlotService {
+	return &service{
+		slots: repo,
+	}
+}
+
+func (service *service) Save(slot entity.Slot) (entity.Slot, error) {
+	err := service.slots.Save(slot)
+	return slot, err
+}
+
+func (service *service) FindAll() []entity.Slot {
+	return service.slots.FindAll()
+}
+
+func (service *service) Find(startTime, date string) (entity.Slot, error) {
+	var slot entity.Slot
+	slot.StartTime = startTime
+	slot.Date = date
+	return service.slots.Find(slot)
+}
+
+func (service *service) GetSlots(slotID uint64) (entity.Slot, error) {
+	var slot entity.Slot
+	slot.ID = slotID
+	return service.slots.GetSlots(slot)
+}
