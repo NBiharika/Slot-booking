5cd133bb83e539a37093feb0e44851e0004acb13
Niharika <niharika.singh6@byjus.com>
2022-07-08 13:18:11 +0530

    new changes for review

---
+++ Middlewares/auth.go
@@ -0,0 +1,24 @@
+package Middlewares
+
+import (
+	"Slot_booking/controller"
+	"github.com/gin-gonic/gin"
+)
+
+func Auth() gin.HandlerFunc {
+	return func(context *gin.Context) {
+		tokenString := context.GetHeader("Authorization")
+		if tokenString == "" {
+			context.JSON(401, gin.H{"error": "request does not contain an access token"})
+			context.Abort()
+			return
+		}
+		err := controller.ValidateToken(tokenString)
+		if err != nil {
+			context.JSON(401, gin.H{"error": err.Error()})
+			context.Abort()
+			return
+		}
+		context.Next()
+	}
+}
